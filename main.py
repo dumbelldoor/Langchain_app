eb1e340e3202cbb4f64f5e31069c7373a6232f34
dumbelldoor <shishirgupta6574@gmail.com>
2023-12-30 13:58:07 +0530

    Streamlit web-app is created

---
+++ main.py
@@ -0,0 +1,28 @@
+import langchain_helper as lch
+import streamlit as stl
+
+stl.title("Pet Name Generator")
+
+
+animal_type = stl.sidebar.selectbox("What is your Pet type:",
+                                    ("Cat", "Dog", "Cow", "Sheep", " Piglet"))
+
+if animal_type == 'Cat':
+    pet_color = stl.sidebar.text_area(label="What is the color of your Cat:", max_chars=15)
+if animal_type == 'Dog':
+    pet_color = stl.sidebar.text_area(label="What is the color of your Dog:", max_chars=15)
+if animal_type == 'Cow':
+    pet_color = stl.sidebar.text_area(label="What is the color of your Cow:", max_chars=15)
+if animal_type == 'Sheep':
+    pet_color = stl.sidebar.text_area(label="What is the color of your Sheep:", max_chars=15)
+if animal_type == 'Piglet':
+    pet_color = stl.sidebar.text_area(label="What is the color of your Piglet:", max_chars=15)
+
+
+
+
+number_of_pet = stl.sidebar.selectbox("Enter the number of pet names you want:", ("1", "2", " 3", "4", "5", "6"))
+name_type = stl.sidebar.selectbox("What Kind of name you want:", ("Cute", "Cool", "Sassy", "Strong"))
+if pet_color:
+    response = lch.Gen_petnames(animal_type, pet_color, number_of_pet,name_type)
+    stl.text(response)
