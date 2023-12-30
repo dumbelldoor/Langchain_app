5a24c514cda11fe227f7cedaf669db04c2e937a0
dumbelldoor <shishirgupta6574@gmail.com>
2023-12-30 12:50:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+
+import os
+
+""" API key Generated """
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+def Gen_petnames(animal_type, pet_color, number_of_names, name_type):
+    llm = OpenAI(temperature=0.7)
+
+    prompt_template_names = PromptTemplate(
+        input_variables=['animal_type', 'pet_color', 'number_of_names', 'name_type'],  # input variable {animal_type}
+        template=f"I want {number_of_names} {animal_type} names, which color is {pet_color} It must be {name_type}and catchy."
+        # prompt template
+    )
+
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_names)  # LLMChain is used to connect LLM's and prompts here
+    # name = llm("I want 5 Dog names, It must be cool and catchy.") #Passed simple string into the LLM
+    response = name_chain(
+        {'animal_type': animal_type, 'pet_color': pet_color}
+    )  # response is being stored in a dictionary
+
+    return response
+
+
+if __name__ == "__main__":
+    print(Gen_petnames("cow", "brown", 4, 'cute'))
